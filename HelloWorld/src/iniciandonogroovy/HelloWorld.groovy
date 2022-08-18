3cb3ebcd11f2f5a5555ea7689d0ef0c7efe01f64
jgmarquesm <joaogabrielmarques@discente.ufg.br>
2022-08-18 01:14:10 -0300

    Progressos

--- HelloWorld/src/iniciandonogroovy/HelloWorld.groovy
+++ HelloWorld/src/iniciandonogroovy/HelloWorld.groovy
@@ -1,4 +1,13 @@
+// Se precisarmos usar um package 3rd party que não vem no download de Java ou Groovy, podemos usar o Annotation Grape
+// Aqui queremos baixar a versão '3.4' do artefato 'commons-lang3' do package 'org.apache.commons'
+@Grapes(
+    @Grab(group='org.apache.commons',module='commons-lang3',version='3.12')
+)
+// Agora é só usar. Simples e rápido, sem precisar ficar baixando e descompactando .jars
+
 package iniciandonogroovy
+
+import groovy.grape.Grape
 
 // O uso de ; é opcional!
 // O uso dos () para passar os argumentos do método também é opcional (isso me parece meio desorganizado)
